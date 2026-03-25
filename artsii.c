572a9f174d36d6d3ca09d044ab114db0199880f3
Yasansvr <yasansvr@outlook.com>
2026-03-26 00:39:21 +0100

    FEAT : Adding drawing features(rectangles and texts) and deleting button

--- artsii.c
+++ artsii.c
@@ -10,7 +10,20 @@ typedef struct window
     int start_y;
 }winCor;
 
+typedef struct rectangle
+{
+    int memx;
+    int memy;
+    int width;
+    int height;
+} Rectangle;
 
+typedef struct text
+{
+    int memx;
+    int memy;
+    char text[100];
+}Text;
 
 int main()
 {
@@ -19,28 +32,65 @@ int main()
 	noecho();//echo swtiched off
     cbreak();
     curs_set(1);
-    
     refresh();
 	keypad(stdscr,TRUE);//function keys reading enabled
     bool exit = false; 
     int cursInitX = 1;
     int cursInitY = 1;
-    int width = 0;
+    int memx;
+    int memy;
     int height = 0;
+    int width = 0;
+    char help[] = "'D' to draw, 'W' to write text, 'C' to clear last rectangle, 'Z' to delete last text, 'Q' to quit";
+    char title[] = "ARTSII";
+    char text[100];
+    
+    Rectangle rects[100];
+    Text texts[100];
+    int rect_count = 0;
+    int text_count = 0;
 	while (!exit)
     {
         int row , col;
         getmaxyx(stdscr,row,col );
         winCor win1 = {row , col , 0 , 0 };
         WINDOW *win = newwin(win1.height , win1.width , win1.start_y , win1.start_x);
+        mvwprintw(win, row-2, (col-strlen(help))/2, "%s", help);
+        mvwprintw(win, 1, (col-strlen(title))/2, "%s", title);
         
         box(win, 0, 0);
         wmove(win, cursInitY, cursInitX);
         wrefresh(win);
-
         keypad(win, TRUE);
 
-        
+
+        //DRAW RECTANGLES
+        for (int i = 0; i < rect_count; i++)
+        {
+            int rx = rects[i].memx;
+            int ry = rects[i].memy;
+            int rw = rects[i].width;
+            int rh = rects[i].height;
+            if (rw > 0 && rh > 0)
+            {
+                mvwhline(win, ry, rx, 0, rw);
+                mvwhline(win, ry + rh - 1, rx, 0, rw);
+                mvwvline(win, ry, rx, 0, rh);
+                mvwvline(win, ry, rx + rw - 1, 0, rh);
+                mvwaddch(win, ry, rx, ACS_ULCORNER);
+                mvwaddch(win, ry, rx + rw - 1, ACS_URCORNER);
+                mvwaddch(win, ry + rh - 1, rx, ACS_LLCORNER);
+                mvwaddch(win, ry + rh - 1, rx + rw - 1, ACS_LRCORNER);
+            }
+        }
+        wmove(win, cursInitY, cursInitX);
+
+        //DRAW TEXTS
+        for (int i = 0; i < text_count; i++)
+        {
+            mvwprintw(win, texts[i].memy, texts[i].memx, "%s", texts[i].text);
+        }
+        wmove(win, cursInitY, cursInitX);
 
 
 
@@ -84,13 +134,17 @@ int main()
             case 'd':
             case 'D':
             {
+                memx = cursInitX;
+                memy = cursInitY;
                 char widthStr[50];
                 char heightStr[50];
                 mvwprintw(win, cursInitY, cursInitX, "ENTER WIDTH:");
                 wrefresh(win);
                 echo();
                 mvwgetnstr(win, cursInitY, cursInitX + 15, widthStr, 49);
+                
                 width = atoi(widthStr);
+                width = width * 2;
                 noecho();
                 mvwprintw(win, cursInitY, cursInitX, "ENTER HEIGHT:");
                 wrefresh(win);
@@ -99,6 +153,38 @@ int main()
                 height = atoi(heightStr);
                 noecho();
                 
+                if (width > 0 && height > 0 && rect_count < 100) {
+                    rects[rect_count].memx = memx;
+                    rects[rect_count].memy = memy;
+                    rects[rect_count].width = width;
+                    rects[rect_count].height = height;
+                    rect_count++;
+                }
+                
+                break;
+            }
+            
+            case 'c':
+            case 'C':
+                rect_count--;
+                break;
+            case 'z':
+            case 'Z':
+                text_count--;
+                break;
+            case 'w':
+            case 'W':
+            {
+                echo();
+                mvwgetnstr(win, cursInitY, cursInitX, text, 99);
+                noecho();
+                if (strlen(text) > 0 && text_count < 100)
+                {
+                    texts[text_count].memx = cursInitX;
+                    texts[text_count].memy = cursInitY;
+                    strcpy(texts[text_count].text, text);
+                    text_count++;
+                }
                 break;
             }
         }
