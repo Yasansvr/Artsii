039a3295d443a8af4f66349284aecef791342865
Yasansvr <yasansvr@outlook.com>
2026-03-25 15:53:37 +0100

    FEAT: Add Arrow Keys trigger for moving curser

--- artsii.c
+++ artsii.c
@@ -16,9 +16,13 @@ int main()
 	raw();//disable inline buffering
 	noecho();//echo swtiched off
     cbreak();
-    curs_set(0);
+    curs_set(1);
+    
+    refresh();
 	keypad(stdscr,TRUE);//function keys reading enabled
     bool exit = false; 
+    int cursInitX = 1;
+    int cursInitY = 1;
 
 	while (!exit)
     {
@@ -26,14 +30,47 @@ int main()
         getmaxyx(stdscr,row,col );
         winCor win1 = {row , col , 0 , 0 };
         WINDOW *win = newwin(win1.height , win1.width , win1.start_y , win1.start_x);
-        refresh();
-        box(win , 0 , 0);
+        
+        box(win, 0, 0);
+        wmove(win, cursInitY, cursInitX);
         wrefresh(win);
-        char c = getch();
-        if (c == 'q' || c == 'Q')
+
+        keypad(win, TRUE);
+        //BUTTON TRIGGER
+        int ch = wgetch(win);
+        if (ch == 'q' || ch == 'Q')
         {
             exit=true;
         }
+
+        switch (ch)
+        {
+            case KEY_RIGHT:
+                if (cursInitX < win1.width - 2)
+                {
+                    cursInitX++;
+                }
+                break;
+            case KEY_LEFT:
+                if (cursInitX > 1)
+                {
+                    cursInitX--;
+                }
+                break;
+            case KEY_UP:
+                if (cursInitY > 1)
+                {
+                    cursInitY--;
+                }
+                break;
+            case KEY_DOWN:
+                if (cursInitY < win1.height - 2)
+                {
+                    cursInitY++;
+                }
+                break;
+        }  
+        
         delwin(win);
         
     }
